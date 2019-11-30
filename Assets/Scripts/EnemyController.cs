3003c218498dc72573d4ebfea867cd469649415f
Arthur Osipyan <arthurosipyan@outlook.com>
2019-11-30 00:52:59 -0500

    enemy additions

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public bool vertical;
+
+    Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = rigidbody2D.position;
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed;
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed;
+        }
+
+        rigidbody2D.MovePosition(position);
+    }
+}
+
