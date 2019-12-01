18cbf46ab75cb89e7a5573aeb8a5713b163b8970
Arthur Osipyan <arthurosipyan@outlook.com>
2019-12-01 13:52:28 -0500

    Update EnemyController.cs

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed;
+    public float speed = 3.0f;
     public bool vertical;
+    public float changeTime = 3.0f;
 
     Rigidbody2D rigidbody2D;
+    float timer;
+    int direction = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer -= timer.deltaTime;
+
+        if (timer < 0){
+            direction = -direction;
+            timer = changeTime;
+        }
+        
         Vector2 position = rigidbody2D.position;
+
         if (vertical)
         {
             position.y = position.y + Time.deltaTime * speed;
@@ -31,4 +43,3 @@ public class EnemyController : MonoBehaviour
         rigidbody2D.MovePosition(position);
     }
 }
-
