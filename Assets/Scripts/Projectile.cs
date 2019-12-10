3e99bc4be6b60090af8019fcb3c6844baf705ab8
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2019-12-10 12:25:26 -0500

    Update Projectile.cs

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
-    // Start is called before the first frame update
+
     void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
